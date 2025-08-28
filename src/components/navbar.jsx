48c2d2aa9b93ff50190f619609354cdfdabd77af
Ekazia88 <taqimfrm@gmail.com>
2025-08-28 23:51:44 +0800

    changes navbar and landing page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,20 @@
-import react from "react";
-
+import React from "react";
+import { FaCaretDown } from "react-icons/fa6";
 const Navbar = () => {
     return(
-        <div className="w-fit relative bg-gray-500 top">
-            <h1>
-                Animview
-            </h1>
+        <div className=" bg-gray-400 box-border flex items-center justify-between px-8 py-4 shadow-xl">
+            <div className="flex items-center justify-start gap-8 w-full font-semibold text-2xl">
+                <h1 className="text-5xl">Animview</h1>
+                <div className="text-white flex pt-3  gap-3 justify-center">
+                    <h1 className="hover:text-black hover:cursor-pointer">Manga</h1>
+                    <a className="flex justify-center items-center gap-1 hover:text-black hover:cursor-pointer">
+                        <h1 className="hover:text-black hover:cursor-pointer">Seasons</h1>
+                        <FaCaretDown/>    
+                    </a>
+                    <h1 className="hover:text-black hover:cursor-pointer">Top 10 Anime</h1>
+                    <h1 className="hover:text-black hover:cursor-pointer">Vote Your Choice</h1>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Landing = () => {
     return(
-        <div className="w-full h-screen bg-blue-400 flex justify-center items-center">
+        <div className="w-full h-screen bg-white-400 flex justify-center items-center">
             <h1 className="text-white text-3xl font-bold">This is Landing Page</h1>
         </div>
     )
