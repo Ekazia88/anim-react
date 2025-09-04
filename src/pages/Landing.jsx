52e7ef1bbd69796789590cd16b5542b49bf9008e
Ekazia88 <taqimfrm@gmail.com>
2025-09-04 21:49:10 +0800

    added season wheel

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,47 +1,58 @@
 import React , {use, useEffect,useState}from "react";
 import { FaAngleLeft } from "react-icons/fa6";
 import { FaAngleRight } from "react-icons/fa6";
-import Jikan from "jikan-api.js";
+
 const Landing = () => {
     const [seasons, setSeasons] = useState([]);
-    console.log(getSeasons);
+    const [currentSeason, setCurrentSeason] = useState(0);
+    console.log(seasons);
     
     useEffect(() => {
         fetch("https://api.jikan.moe/v4/seasons")
             .then((response) => response.json())
-            .then((data) => setSeasons(data.data))
+            .then((data) => {
+                const flat =[];
+                data.data
+                    .forEach((item) => {
+                        item.seasons.forEach((season) => {
+                            flat.push({year: item.year, season: season});
+                        });
+                    });
+                setSeasons(flat);
+        })
             .catch((error) => console.error("Error fetching data:", error));
     }, []);
-    const [currentSeason, setCurrentSeason] = useState(0);
-    const handlePrevSeason = () => {
-        setCurrentSeason((prevSeason) => (prevSeason === 0 ? seasons.length -   1 : prevSeason - 1));
+
+    const handlePrev = () => {
+        setCurrentSeason(prev => (prev === 0 ? seasons.length - 1 : prev - 1));
     };
-    const handleNextSeason = () => {
-        setCurrentSeason((prevSeason) => (prevSeason === seasons.length - 1 ? 0 : prevSeason + 1));
+     const handleNext = () => {
+        setCurrentSeason(prev => (prev === seasons.length - 1 ? 0 : prev + 1));
     };
+    
     const currentSeasonData = seasons[currentSeason];
-        console.log(currentSeasonData.season_name);
+    console.log(currentSeasonData);
     return(
         <div className="w-full h-screen bg-blue-500">
             <div className="flex bg-amber-400 w-full px-5 py-5">
             
                 
-                <div className="flex items-center gap-20">
-                    <FaAngleLeft className="text-4xl text-red-500 cursor-pointer" onClick={handlePrevSeason}/>
+                <div className="flex items-center gap-5">
+                    <FaAngleLeft className="text-4xl text-red-500 cursor-pointer" onClick={handlePrev}/>
                       
                 <div className="justify-center ">
-                    {currentSeasonData? (
-                            <h1 className="text-white text-6xl">
-                            {currentSeasonData.season_name}
-                            </h1>
-                    ) :(
+                    {currentSeasonData ?(
+                        <h1 className="text-white text-4xl capitalize">
+                            {currentSeasonData.season} {currentSeasonData.year}
+                        </h1>
+                    ): (
                         <h1>Loading...</h1>
                     )}
                    
                     <h1 className="text-gray-600">July 2025 - September 2025 </h1>
                 </div>
                     <FaAngleRight className="text-4xl text-red-500 cursor-pointer" 
-                    onClick={handleNextSeason}/>
+                    onClick={handleNext}/>
                 </div>
             </div>
            
